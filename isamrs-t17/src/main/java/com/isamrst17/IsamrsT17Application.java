948507d667bd46b34cd59f295646b5e8cb869769
Dejan <dejangacevic@gmail.com>
2018-05-09 08:51:23 +0200

    project setup

---
+++ isamrs-t17/src/main/java/com/isamrst17/IsamrsT17Application.java
@@ -0,0 +1,12 @@
+package com.isamrst17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsamrsT17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsamrsT17Application.class, args);
+	}
+}
