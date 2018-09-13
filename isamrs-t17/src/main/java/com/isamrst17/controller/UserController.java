ee15e6ba750791b18be11289eba930b2786fb649
Srbislav <srbo_m12@hotmail.com>
2018-09-13 18:05:15 +0200

    Login functionality implemented

--- isamrs-t17/src/main/java/com/isamrst17/controller/UserController.java
+++ isamrs-t17/src/main/java/com/isamrst17/controller/UserController.java
@@ -3,19 +3,29 @@ package com.isamrst17.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.isamrst17.dto.LoginDTO;
 import com.isamrst17.dto.MessageDTO;
 import com.isamrst17.dto.RegisterDTO;
 import com.isamrst17.model.Address;
 import com.isamrst17.model.City;
 import com.isamrst17.model.User;
+import com.isamrst17.security.TokenUtils;
 import com.isamrst17.service.AddressService;
 import com.isamrst17.service.CityService;
+import com.isamrst17.service.UserDetailsServiceImpl;
 import com.isamrst17.service.UserService;
 
 
@@ -32,7 +42,21 @@ public class UserController {
 	
 	@Autowired
 	UserService userService;
+	
+	@Autowired
+	AuthenticationManager authenticationManager;
+	
+	@Autowired
+	UserDetailsService userDetailsService;
 
+	@Autowired
+	UserDetailsServiceImpl userDetailsServiceImpl;
+	
+	@Autowired
+	TokenUtils tokenUtils;
+
+	
+	//Register
 	@RequestMapping(value = "/register", method = RequestMethod.POST, consumes = "application/json")
 	public ResponseEntity<MessageDTO> register(@RequestBody RegisterDTO registerDTO) {
 		MessageDTO messageDTO = new MessageDTO();
@@ -61,4 +85,40 @@ public class UserController {
 		}
 		return new ResponseEntity<>(messageDTO, HttpStatus.CREATED);
 	}
+	
+	//Login
+	@RequestMapping(value = "/login", method = RequestMethod.POST, consumes = "application/json")
+	  public ResponseEntity<MessageDTO> login(@RequestBody LoginDTO loginDTO) {
+	    try {
+	      UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(loginDTO.getUsername(),
+	          loginDTO.getPassword());
+	      Authentication authentication = authenticationManager.authenticate(token);
+	      SecurityContextHolder.getContext().setAuthentication(authentication);
+
+	      UserDetails details = userDetailsService.loadUserByUsername(loginDTO.getUsername());
+	      User user = userService.findByUsername(details.getUsername());
+	      System.out.println(user.getUsername());
+	      MessageDTO m = new MessageDTO();
+
+	      m.setId(user.getId());
+	      m.setJwt(tokenUtils.generateToken(details));
+	      m.setRola("USER");
+
+	      System.out.println(user.getFirstName() +  user.getLastName());
+	      System.out.println(SecurityContextHolder.getContext().getAuthentication().getName());
+
+	      return new ResponseEntity<>(m, HttpStatus.OK);
+	    } catch (Exception e) {
+	      MessageDTO m = new MessageDTO();
+	      m.setError("Wrong username/password!");
+	      return new ResponseEntity<>(m, HttpStatus.NOT_FOUND);
+	    }
+	  }
+	
+	//Login check
+	@RequestMapping(value = "/login/check/{username}", method = RequestMethod.GET)
+	  public Boolean loginCheck(@PathVariable String username) {
+	    User user = userService.findByUsername(username);
+	    return userService.findAll().contains(user);
+	 }
 }
