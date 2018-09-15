5ddec36e3a80fae2bf80459d549a81eeb5184730
Vladimir <djvlada03@gmail.com>
2018-09-15 13:52:03 +0200

    Added cinema and playhouse pages and scripts for displaying all cinemas/playhouses.

---
+++ isamrs-t17/src/main/resources/static/js/custom/theatre.js
@@ -0,0 +1,88 @@
+// Get all theatres
+
+// $('.card-wrapper').ready(function() {
+//   $.ajax({
+//     url: "http://localhost:8080/api/user/theatres/all",
+//     type: "GET",
+//     headers: {"Authorization": localStorage.jwt},
+//     success: function(data) {
+//       $('.card-wrapper').append(data);
+//     }
+//   })
+// });
+
+// Get all theatres 2
+
+function getStars(rating) {
+  debugger;
+  var output = "";
+  if (rating == 1) {
+    output = '<i class="zmdi zmdi-star-outline"></i><i class="zmdi zmdi-star-outline"></i><i class="zmdi zmdi-star-outline"></i><i class="zmdi zmdi-star-outline"></i><i class="zmdi zmdi-star"></i>';
+  } else if (rating > 1 && rating < 2) {
+    output = '<i class="zmdi zmdi-star-outline"></i><i class="zmdi zmdi-star-outline"></i><i class="zmdi zmdi-star-outline"></i><i class="zmdi zmdi-star-half"></i><i class="zmdi zmdi-star"></i>';
+  } else if (rating == 2) {
+    output = '<i class="zmdi zmdi-star-outline"></i><i class="zmdi zmdi-star-outline"></i><i class="zmdi zmdi-star-outline"></i><i class="zmdi zmdi-star"></i><i class="zmdi zmdi-star"></i>';
+  } else if (rating > 2 && rating <3) {
+    output = '<i class="zmdi zmdi-star-outline"></i><i class="zmdi zmdi-star-outline"></i><i class="zmdi zmdi-star-half"></i><i class="zmdi zmdi-star"></i><i class="zmdi zmdi-star"></i>';
+  } else if (rating == 3) {
+    output = '<i class="zmdi zmdi-star-outline"></i><i class="zmdi zmdi-star-outline"></i><i class="zmdi zmdi-star"></i><i class="zmdi zmdi-star"></i><i class="zmdi zmdi-star"></i>';
+  } else if (rating > 3 && rating < 4) {
+    output = '<i class="zmdi zmdi-star-outline"></i><i class="zmdi zmdi-star-half"></i><i class="zmdi zmdi-star"></i><i class="zmdi zmdi-star"></i><i class="zmdi zmdi-star"></i>';
+  } else if (rating == 4) {
+    output = '<i class="zmdi zmdi-star-outline"></i><i class="zmdi zmdi-star"></i><i class="zmdi zmdi-star"></i><i class="zmdi zmdi-star"></i><i class="zmdi zmdi-star"></i>';
+  } else if (rating > 4 && rating < 5) {
+    output = '<i class="zmdi zmdi-star-half"></i><i class="zmdi zmdi-star"></i><i class="zmdi zmdi-star"></i><i class="zmdi zmdi-star"></i><i class="zmdi zmdi-star"></i>';
+  } else if (rating == 5) {
+    output = '<i class="zmdi zmdi-star"></i><i class="zmdi zmdi-star"></i><i class="zmdi zmdi-star"></i><i class="zmdi zmdi-star"></i><i class="zmdi zmdi-star"></i>';
+  } else {
+    output = '<i class="zmdi zmdi-star-outline"></i><i class="zmdi zmdi-star-outline"></i><i class="zmdi zmdi-star-outline"></i><i class="zmdi zmdi-star-outline"></i><i class="zmdi zmdi-star-outline"></i>';
+  }
+  return output;
+}
+
+$('.card-wrapper').ready(function() {
+  debugger;
+  $.ajax({
+    url: "http://localhost:8080/api/user/theatres",
+    type: "GET",
+    // headers: {"Authorization": localStorage.jwt},
+    success: function(data) {
+      console.log("FU");
+      $.each(data, function(i) {
+        var theatre = data[i];
+        console.log(theatre);
+        console.log(theatre.name);
+        console.log(theatre.rating);
+        console.log(getStars(theatre.rating));
+        $('.card-wrapper').append('<div class="container-card100">\n'
+            + '                  <div class="wrap-card100">\n'
+            + '                    <span class="card100-form-title cardTitle" id="cardTitle">\n'
+            + '                      '+ theatre.name + '\n'
+            + '                    </span>\n'
+            + '                    <div class="card-data">\n'
+            + '                      <span class="card100-form-text">\n'
+            + '                        '+ theatre.address.address + ', ' + theatre.address.city + '\n'
+            + '                      </span>\n'
+            + '                      <span class="card100-form-text">\n'
+            + '                        '+ theatre.desc + '\n'
+            + '                      </span>\n'
+            + '                    </div>\n'
+            + '                    <div class="rating">\n'
+            // + '                      <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>\n'
+            + getStars(theatre.rating)
+            + '                    </div>\n'
+            + '\n'
+            + '                    <div class="container-card100-form-btn repertoire-cont" id="repertoire-cont">\n'
+            + '                      <div class="wrap-content100-form-btn">\n'
+            + '                        <div class="topbar100-form-bgbtn"></div>\n'
+            + '                        <button class="card100-form-btn repertoire-button" type="submit" id="repertoire-button">\n'
+            + '                          REPERTOIRE\n'
+            + '                        </button>\n'
+            + '                      </div>\n'
+            + '                    </div>\n'
+            + '                  </div>\n'
+            + '                </div>');
+      })
+    }
+  })
+});
